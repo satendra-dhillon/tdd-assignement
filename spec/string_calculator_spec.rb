b4c60da854fb37520c589dbd7a76ace07aa37a79
Satendra Singh <satendra.singh3@wework.com>
2025-05-09 14:16:07 +0530

    Added class and respective rspec file

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+	describe '#add' do
+
+		context 'when input is blank string' do
+		end
+
+		context 'when input is not blank stirng' do
+		end
+
+	end
+end
