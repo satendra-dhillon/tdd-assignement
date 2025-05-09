82481daf039e8b3c0c5d17961c377215056fe4fa
Satendra Singh <satendra.singh3@wework.com>
2025-05-09 14:20:03 +0530

    Added first example for test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe StringCalculator do
 	describe '#add' do
 
 		context 'when input is blank string' do
+			it 'return 0 if blank input string' do
+				string_calculator = StringCalculator.new
+      			expect(string_calculator.add("")).to eq(0)
+			end
 		end
 
 		context 'when input is not blank stirng' do
