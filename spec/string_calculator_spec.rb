ef8b7430bd968e45b03ac1ec98790b8449a8cea9
Satendra Singh <satendra.singh3@wework.com>
2025-05-09 18:51:25 +0530

    Added rspec along with logic for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+	let(:string_calculator) { StringCalculator.new }
+	
 	describe '#add' do
 
 		context 'when input is empty string' do
 			it 'return 0 if empty input string' do
-				string_calculator = StringCalculator.new
       			expect(string_calculator.add("")).to eq(0)
 			end
 		end
@@ -13,18 +14,31 @@ RSpec.describe StringCalculator do
 		context 'when input is not empty stirng' do
 
 			it 'return number as integer when single number pased as input string' do
-				string_calculator = StringCalculator.new
       			expect(string_calculator.add("5")).to eq(5)
 			end
 
 			it 'returns sum for non-empty string with comma separated number' do
-				string_calculator = StringCalculator.new
 				expect(string_calculator.add("2,3")).to eq(5)
 		    end
 
+		    it 'returns the sum of multiple numbers separated by commas' do
+		    	expect(string_calculator.add("1,2,3,4")).to eq(10)
+		    end
+
+		    it 'returns the sum of numbers separated by newlines' do
+				expect(string_calculator.add("1\n2,3")).to eq(6)
+		    end
+
 		    it 'returns sum for non-empty string with comma separated numbers or separated with \n' do
-				string_calculator = StringCalculator.new
 				expect(string_calculator.add("1,2\n3")).to eq(6)
+		    end
+
+		    it 'raises an exception when called with a negative number' do
+				expect { string_calculator.add("-1,2") }.to raise_error(RuntimeError, "negative numbers not allowed -1")
+		    end
+
+		    it 'shows all negative numbers in the exception message' do
+				expect { string_calculator.add("-1,2,-3") }.to raise_error(RuntimeError, "negative numbers not allowed -1,-3")
 		    end
 		end
 	end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,18 @@
 class StringCalculator
 	def add(input)
 		return 0 if input.empty?
-		return input.to_i if !input.include?(',')
+		regex = /[,\n]/
 
-		split_array = input.split(/[,\n]/)
-		return split_array.map(&:to_i).sum
+		return input.to_i if !input.match?(regex)
+
+		split_array = input.split(regex)
+		integers = split_array.map(&:to_i)
+
+		negatives = integers.select { |n| n < 0 }
+	    if negatives.any?
+	      raise "negative numbers not allowed #{negatives.join(',')}"
+	    end
+
+		return integers.map(&:to_i).sum
 	end
 end
