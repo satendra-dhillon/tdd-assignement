8310dbfb10a7fb5fb75ec7d1d392a3f013fb136b
Satendra Singh <satendra.singh3@wework.com>
2025-05-09 14:23:28 +0530

    Handle first rspec test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,14 +3,14 @@ require_relative '../string_calculator'
 RSpec.describe StringCalculator do
 	describe '#add' do
 
-		context 'when input is blank string' do
-			it 'return 0 if blank input string' do
+		context 'when input is empty string' do
+			it 'return 0 if empty input string' do
 				string_calculator = StringCalculator.new
       			expect(string_calculator.add("")).to eq(0)
 			end
 		end
 
-		context 'when input is not blank stirng' do
+		context 'when input is not empty stirng' do
 		end
 
 	end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+	def add(input)
+		return 0 if input.empty?
+	end
 end
